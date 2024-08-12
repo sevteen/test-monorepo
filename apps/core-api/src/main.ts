b33f6344b508cc681cb5135fd93f91699239ece6
Hanifjubelio <ilhamh.hanif@jubelio.com>
2024-08-12 20:49:09 +0700

    feat(security): add fastify security plugins
    - Integrated @fastify/helmet for setting various HTTP headers for security.
    - Added @fastify/cookie
    for cookie handling.
    - Implemented @fastify/csrf-protection for CSRF protection.
    - Included
    @fastify/cors for managing CORS settings.
    - Configured NestJS application with these Fastify
    plugins to enhance security.
    Updated bootstrap function to include security-related middleware and
    services.

--- apps/core-api/src/main.ts
+++ apps/core-api/src/main.ts
@@ -1,4 +1,9 @@
+import fastifyCookie from '@fastify/cookie';
+import fastifyCors from '@fastify/cors';
+import fastifyCsrf from '@fastify/csrf-protection';
+import fastifyHelmet from '@fastify/helmet';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
@@ -6,16 +11,22 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   console.log(process.env);
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  // app.register(fastifyHelmet, {});
+  await app.register(fastifyHelmet);
+  await app.register(fastifyCookie);
+  await app.register(fastifyCsrf);
+  await app.register(fastifyCors);
 
   // app.useLogger(app.resolve(LoggerService));
 
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.enableShutdownHooks();
 
-  const port = process.env.CORE_API_PORT || 3000;
+  const envConfig = app.get(ConfigService);
+
+  const port = envConfig.get('CORE_API_PORT');
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix} `);
+  Logger.log(`🚀 Application is running on:  ${await app.getUrl()}`);
 }
 
 bootstrap();
