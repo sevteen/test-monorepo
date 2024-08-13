1eb346a4e1f9405f335e50764faee4db758ffae9
sevteen <ih.sevteen@gmail.com>
2024-08-13 13:55:52 +0700

    feat(api@assert): add type assertation

---
+++ apps/core-api/src/common/type/CommonTypes.ts
@@ -0,0 +1,6 @@
+export type Nullable<T> = T | null;
+export type Optional<T> = T | undefined;
+export type StatusCodeDescription = {
+  code: number;
+  message: string;
+};
---
+++ apps/core-api/src/common/utilities/assert/CoreAssert.ts
@@ -0,0 +1,22 @@
+import { Nullable, Optional } from '@common/type/CommonTypes';
+
+export class CoreAssert {
+  public static isTrue(expression: boolean, exception: Error): void {
+    if (!expression) {
+      throw exception;
+    }
+  }
+
+  public static isFalse(expression: boolean, exception: Error): void {
+    if (expression) {
+      throw exception;
+    }
+  }
+
+  public static notEmpty<T>(value: Optional<Nullable<T>>, exception: Error): T {
+    if (value === null || value === undefined) {
+      throw exception;
+    }
+    return value;
+  }
+}
