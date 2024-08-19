32ae3d988bbbe9c60b7635cfb976bf6f26953992
sevteen <ih.sevteen@gmail.com>
2024-08-19 09:53:43 +0700

    refactor(validation): refactor validation pipe

---
+++ apps/core-api/src/common/pipes/ZodValidationPipe.ts
@@ -0,0 +1,13 @@
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
+import { ZodSchema } from 'zod';
+
+@Injectable()
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: ZodSchema) {}
+
+  transform(value: unknown, metadata: ArgumentMetadata) {
+    return this.schema.parse(value);
+
+    // Catching error related to exception filter
+  }
+}
